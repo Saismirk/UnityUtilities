7cd4b51544519b2524e5732de03fab3573059377
Saismirk <ndgonzal@uwaterloo.ca>
2021-05-18 00:18:32 -0500

    Timer added

---
+++ Assets/Utilities/Timer/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+#endif
+namespace Utilities {
+    public class Timer : Singleton<Timer> {
+        public static List<TimerInstance> timers =  new List<TimerInstance>();
+        void Update() {
+            RemoveCompletedTimers(ref timers);
+            foreach (var timer in timers) {
+                if (!timer.IsPaused) timer.Tick(Time.deltaTime);
+            }
+        }
+        /// <summary>Creates a countdown Timer. Triggers callback when timer is up.</summary>
+        ///<param name = "duration"> Duration in seconds of the timer.</param>
+        ///<param name = "delay"> Delay in seconds of the timer (default 0).</param>
+        public static TimerInstance Countdown (float duration, float delay = 0) => new TimerInstance(duration, delay);
+        public static void CancelAllTimers() {
+            foreach (var timer in timers) {
+                timer.Cancel();
+            };
+        }
+        public static void ForceCompleteAllTimers() {
+            foreach (var timer in timers) {
+                timer.ForceComplete();
+            };
+        }
+        void RemoveCompletedTimers (ref List<TimerInstance> timerList) {
+            if (timerList == null || timerList.Count == 0) return;
+            var tempList = new List<TimerInstance>();
+            foreach (var timer in timers) {
+                if (timer.TimeLeft > 0) tempList.Add(timer);
+            }
+            timerList = tempList;
+        }
+        public static bool IsRunning(TimerInstance timer) => timers.Contains(timer);
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void StartUp() => Instance.Initialize();
+        void Initialize(){}
+    }
+}
---
+++ Assets/Utilities/Timer/TimerInstance.cs
@@ -0,0 +1,40 @@
+using System;
+namespace Utilities {
+    public class TimerInstance {
+        event Action OnTimeUp;
+        event Action<float> OnTick;
+        public float TimeLeft {get; private set;}
+        public float Delay {get; private set;}
+        public bool IsPaused {get; private set;}
+        public TimerInstance (float duration, float delay = 0) {
+            this.TimeLeft = duration;
+            this.Delay = delay;
+            Timer.timers.Add(this);
+        }
+        public void Tick(float time) {
+            TimeLeft -= time;
+            if (TimeLeft <= 0 && TimeLeft > -1) {
+                OnTimeUp?.Invoke();
+            } else if(OnTick != null && TimeLeft > 0) OnTick(TimeLeft);
+        }
+        public TimerInstance SetOnTimeUp (Action OnTimeUp) {
+            this.OnTimeUp = OnTimeUp;
+            return this;
+        }
+        public TimerInstance SetOnTick (Action<float> OnTick) {
+            this.OnTick = OnTick;
+            return this;
+        }
+        public void Cancel () => TimeLeft = -2;
+        public void ForceComplete () {
+            TimeLeft = -0.1f;
+            OnTimeUp?.Invoke();
+        }
+        public void Pause() {
+            IsPaused = true;
+        }
+        public void Resume() {
+            IsPaused = false;
+        }
+    }
+}
