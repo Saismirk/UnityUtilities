cfa1e1303138905febe1439bd0676d6a2b904ceb
Saismirk <ndgonzal@uwaterloo.ca>
2021-05-19 23:12:40 -0500

    Update Singleton.cs

--- Assets/Utilities/Singleton.cs
+++ Assets/Utilities/Singleton.cs
@@ -4,61 +4,35 @@
 /// Inherit from this base class to create a singleton.
 /// e.g. public class MyClassName : Singleton<MyClassName> {}
 /// </summary>
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    // Check to see if we're about to be destroyed.
-    private static bool m_ShuttingDown = false;
-    private static object m_Lock = new object();
-    private static T m_Instance;
- 
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    static bool m_ShuttingDown = false;
+    static readonly object m_Lock = new object();
+    static T m_Instance;
     /// <summary>
     /// Access singleton instance through this propriety.
     /// </summary>
-    public static T Instance
-    {
-        get
-        {
-            if (m_ShuttingDown)
-            {
+    public static T Instance {
+        get {
+            if (m_ShuttingDown) {
                 Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
                     "' already destroyed. Returning null.");
                 return null;
             }
- 
-            lock (m_Lock)
-            {
-                if (m_Instance == null)
-                {
-                    // Search for existing instance.
+            lock (m_Lock) {
+                if (m_Instance == null) {
                     m_Instance = (T)FindObjectOfType(typeof(T));
- 
-                    // Create new instance if one doesn't already exist.
-                    if (m_Instance == null)
-                    {
-                        // Need to create a new GameObject to attach the singleton to.
-                        var singletonObject = new GameObject();
-                        m_Instance = singletonObject.AddComponent<T>();
-                        singletonObject.name = typeof(T).ToString() + " (Singleton)";
- 
-                        // Make instance persistent.
-                        DontDestroyOnLoad(singletonObject);
-                    }
+                    if (m_Instance == null) CreateMonoBehaviourInstance(ref m_Instance);
                 }
- 
                 return m_Instance;
             }
         }
     }
- 
- 
-    private void OnApplicationQuit()
-    {
-        m_ShuttingDown = true;
+    static void CreateMonoBehaviourInstance<V>(ref V instance) where V : MonoBehaviour {
+        var singletonObject = new GameObject();
+        instance = singletonObject.AddComponent<V>();
+        singletonObject.name = typeof(V).ToString() + " (Singleton)";
+        DontDestroyOnLoad(singletonObject);
     }
- 
- 
-    private void OnDestroy()
-    {
-        m_ShuttingDown = true;
-    }
+    void OnApplicationQuit() => m_ShuttingDown = true;
+    void OnDestroy() => m_ShuttingDown = true;
 }
