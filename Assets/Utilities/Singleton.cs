52772d579b6d5da837a6435b0ce4863163efee4a
Saismirk <ndgonzal@uwaterloo.ca>
2021-05-19 23:31:52 -0500

    Update Singleton.cs

--- Assets/Utilities/Singleton.cs
+++ Assets/Utilities/Singleton.cs
@@ -9,29 +9,29 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
     static readonly object m_Lock = new object();
     static T m_Instance;
     /// <summary>
-    /// Access singleton instance through this propriety.
+    /// The singleton instance.
     /// </summary>
     public static T Instance {
         get {
             if (m_ShuttingDown) {
-                Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
-                    "' already destroyed. Returning null.");
+                Debug.LogWarning("[Singleton] Instance '" + typeof(T) + "' already destroyed. Returning null.");
                 return null;
             }
             lock (m_Lock) {
                 if (m_Instance == null) {
                     m_Instance = (T)FindObjectOfType(typeof(T));
-                    if (m_Instance == null) CreateMonoBehaviourInstance(ref m_Instance);
+                    m_Instance ??= CreateMonoBehaviourInstance<T>();
                 }
                 return m_Instance;
             }
         }
     }
-    static void CreateMonoBehaviourInstance<V>(ref V instance) where V : MonoBehaviour {
+    static V CreateMonoBehaviourInstance<V>() where V : MonoBehaviour {
         var singletonObject = new GameObject();
-        instance = singletonObject.AddComponent<V>();
+        var instance = singletonObject.AddComponent<V>();
         singletonObject.name = typeof(V).ToString() + " (Singleton)";
         DontDestroyOnLoad(singletonObject);
+        return instance;
     }
     void OnApplicationQuit() => m_ShuttingDown = true;
     void OnDestroy() => m_ShuttingDown = true;
