cfa73bc0393d7e6836423331052a37158c27ebdf
Saismirk <ndgonzal@uwaterloo.ca>
2021-06-16 15:34:27 -0500

    Update Singleton.cs

--- Assets/Utilities/Singleton.cs
+++ Assets/Utilities/Singleton.cs
@@ -4,7 +4,7 @@
 /// Inherit from this base class to create a singleton.
 /// e.g. public class MyClassName : Singleton<MyClassName> {}
 /// </summary>
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
     static bool m_ShuttingDown = false;
     static readonly object m_Lock = new object();
     static T m_Instance;
@@ -19,12 +19,17 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
             }
             lock (m_Lock) {
                 if (m_Instance == null) {
-                    m_Instance = (T)FindObjectOfType(typeof(T));
+                    m_Instance = FindSingletonInstance<T>();
                     m_Instance ??= CreateMonoBehaviourInstance<T>();
                 }
                 return m_Instance;
             }
         }
+    }
+    static V FindSingletonInstance<V>() where V : MonoBehaviour{
+        var instance = (V)FindObjectOfType(typeof(V));
+        DontDestroyOnLoad(instance.gameObject);
+        return instance;
     }
     static V CreateMonoBehaviourInstance<V>() where V : MonoBehaviour {
         var singletonObject = new GameObject();
