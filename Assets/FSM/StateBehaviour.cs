bf604ec2946c10e166bde47b595badf349a154af
Saismirk <ndgonzal@uwaterloo.ca>
2021-08-04 17:52:38 -0500

    Moved FSM to own project

--- Assets/FSM/StateBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace FSM {
-    public abstract class StateBehaviour : ScriptableObject {
-        public virtual void OnEnter(StateMachineRuntime stateMachine, StateMachineController stateMachineController, State state) {}
-        public virtual void OnUpdate(StateMachineRuntime stateMachine, StateMachineController stateMachineController, State state) {}
-        public virtual void OnExit(StateMachineRuntime stateMachine, StateMachineController stateMachineController, State state) {}
-    }
-}
-
